f8758ad2a174a766fc1d4f217fa167f606b60347
Matt Cooksey <mcooksey@csa1.com>
2015-11-06 16:38:46 -0600

    refactor code

--- src/SimUPS.scala
+++ src/SimUPS.scala
@@ -2,44 +2,33 @@
   * Created by mcooksey on 10/29/2015.
   */
 
-import java.io.InputStream
-
 import scala.io.Source
-import scala.collection.mutable.ListBuffer
 import scala.collection.mutable
 
 class Hub(val city: String, val state: String, var id:String) {
   def this(city:String, state:String) = {
-    this(city, state, city + "-" + state)
+    this(city, state, (city + "-" + state).toLowerCase())
+  }
+  override def toString = {
+    s"city: $city, state:$state"
   }
 }
 
-class Hop(val hub1: Hub, val hub2: Hub, val hours: Int, val minutes: Int, val miles: Int) {
+class Hop(val hub1: Hub, val hub2: Hub, val hours: Int, val minutes: Int, val miles: Int, var id:String) {
+  def this(hub1: Hub, hub2: Hub, hours: Int, minutes: Int, miles: Int) = {
+    this(hub1, hub2, hours, minutes, miles, hub1.id + ">" + hub2.id)
+  }
+  override def toString = {
+    s"id: $id, hours: $hours, minutes: $minutes, miles: $miles"
+  }
 }
-
-
 
 object SimUPS {
-  var hubListDijkstra = new mutable.HashMap[String, List[(Double,String)]]()
-  var hubs = new mutable.ListBuffer[Hub]
-  var hops = new mutable.ListBuffer[Hop]
-
-  type Path[Key] = (Double, List[Key])
-
-  def Dijkstra[Key](lookup: mutable.HashMap[Key, List[(Double, Key)]], fringe: List[Path[Key]], dest: Key, visited: Set[Key]): Path[Key] = fringe match {
-    case (dist, path) :: fringe_rest => path match {case key :: path_rest =>
-      if (key == dest) (dist, path.reverse)
-      else {
-        val paths = lookup(key).flatMap {case (d, key) => if (!visited.contains(key)) List((dist + d, key :: path)) else Nil}
-        val sorted_fringe = (paths ++ fringe_rest).sortWith {case ((d1, _), (d2, _)) => d1 < d2}
-        Dijkstra(lookup, sorted_fringe, dest, visited + key)
-      }
-    }
-    case Nil => (0, List())
-  }
+  var hubs = new mutable.HashMap[String, Hub]
+  var hops = new mutable.HashMap[String, Hop]
 
   def main(args: Array[String]) = {
-    hubListDijkstra = initializeHubList("C:\\TEMP\\hubs.csv")
+    initializeHubList("C:\\Users\\mcooksey\\Documents\\GitHub\\SimUPS\\hubs.csv")
 
     var continue = true
 
@@ -66,11 +55,16 @@ object SimUPS {
         case '4' => showAllHubs()
         case '5' => routePackage()
         case '6' => continue = false
+        case '7' => debug()
         case _ => println("Invalid Entry")
       }
       println()
 
     }
+  }
+
+  def debug() = {
+    hops.foreach{case(k,v) => println(k, v.toString)}
   }
 
   def addHop() = {
@@ -92,47 +86,39 @@ object SimUPS {
 
     val hub1 = new Hub(originCity, originState)
     val hub2 = new Hub(destinationCity, destinationState)
-    val path = new Hop(hub1, hub2, hours, minutes, miles)
-    val path2 = new Hop(hub2, hub1, hours, minutes, miles)
 
-    if (hubListDijkstra.keySet.exists(_ == hub1.id)) {
-      var tempList = new ListBuffer[(Double, String)]()
-      tempList = hubListDijkstra(hub1.id).to[ListBuffer]
-      val tuple2 = new Tuple2(path.miles.toDouble, hub2.id)
-      tempList += tuple2
-      hubListDijkstra.put (hub1.id,tempList.to[List])
+    putHop(hub1, hub2, hours, minutes, miles)
+  }
+
+  def putHop(hub1:Hub, hub2:Hub, hours:Int, minutes:Int, miles:Int) {
+    val hop = new Hop(hub1, hub2, hours, minutes, miles)
+    val hopBack = new Hop(hub2, hub1, hours, minutes, miles)
+
+    if (!hubs.keySet.exists(_ == hub1.id)) {
+      hubs.put(hub1.id, hub1)
     }
+    hops.put(hop.id, hop)
 
-    else {
-      hubListDijkstra.put (hub1.id,List((path.miles.toDouble, hub2.id)))
-      hubs += hub1
+    if (!hubs.keySet.exists(_== hub2.id)) {
+      hubs.put(hub2.id, hub2)
     }
-
-    hops += path
-
-    if (hubListDijkstra.keySet.exists(_== hub2.id)) {
-      var tempList = new ListBuffer[(Double,String)]()
-      tempList = hubListDijkstra(hub2.id).to[ListBuffer]
-      val tuple2 = new Tuple2(path.miles.toDouble, hub1.id)
-      tempList += tuple2
-      hubListDijkstra.put (hub2.id,tempList.to[List])
-    }
-    else {
-      hubListDijkstra.put(hub2.id, List((path.miles.toDouble, hub1.id)))
-      hubs += hub2
-    }
-
-    hops += path2
+    hops.put(hopBack.id, hopBack)
   }
 
   def deleteHub() = {
     println("\n*******DELETE HUB*********")
     print("Enter hub to delete: ")
     val hub = scala.io.StdIn.readLine().toLowerCase()
-    if (hubListDijkstra.keySet.exists(_ == hub)) {
-      hubListDijkstra.remove(hub)
-
-      println("Hub deleted.")
+    if (hubs.keySet.exists(_ == hub)) {
+      hubs.remove(hub)
+      hops.foreach{
+        case(key, hop) => {
+          if (hop.hub1.id.equals(hub) || hop.hub2.id.equals(hub)) {
+            hops.remove(key)
+          }
+        }
+      }
+      println("Hub and all associated hops deleted.")
     }
     else {
       println("Hub does not exist.")
@@ -150,7 +136,7 @@ object SimUPS {
       option match {
         case 'y' => {
           //1hubs.clear()
-          hubListDijkstra = initializeHubList("C:\\TEMP\\hubs.csv")
+          initializeHubList("C:\\Users\\mcooksey\\Documents\\GitHub\\SimUPS\\hubs.csv")
           println("Hubs reloaded.")
           continue = false
         }
@@ -162,8 +148,8 @@ object SimUPS {
 
   def showAllHubs() = {
     println("\n******Available Hubs******")
-    println(s"*********${hubListDijkstra.size} TOTAL*********")
-    hubListDijkstra.foreach {case (k,v) => println(k)}
+    println(s"*********${hubs.size} TOTAL*********")
+    hubs.foreach {case (key,hub) => println(hub.city + ", " + hub.state)}
   }
 
   def routePackage() = {
@@ -175,7 +161,7 @@ object SimUPS {
         do {
           print("Enter an origin: ")
           origin = scala.io.StdIn.readLine().toLowerCase()
-          if (hubListDijkstra.keySet.exists(_ == origin)) {
+          if (hubs.keySet.exists(_ == origin)) {
             validOrigin = true
           }
           else {
@@ -186,7 +172,7 @@ object SimUPS {
         do {
           print("Enter a destination: ")
           destination = scala.io.StdIn.readLine().toLowerCase()
-          if (hubListDijkstra.keySet.exists(_ == destination)) {
+          if (hubs.keySet.exists(_ == destination)) {
             validDestination = true
           }
           else {
@@ -194,7 +180,7 @@ object SimUPS {
           }
         } while (!validDestination)
 
-        val shortestPath = Dijkstra[String](hubListDijkstra, List((0.0, List(origin))), destination, Set())
+        val shortestPath = Dijkstra.getShortestPath(hops, origin, destination)
 
         val path = shortestPath._2.to[List]
         val totalMiles = shortestPath._1.toDouble
@@ -212,41 +198,19 @@ object SimUPS {
   }
 
 
-  def initializeHubList(inFile:String):mutable.HashMap[String, List[(Double, String)]] = {
+  def initializeHubList(inFile:String) = {
     val source = Source.fromFile(inFile)
     for (line <- source.getLines()) {
       val cols = line.split(",").map(_.trim)
       // do whatever you want with the columns here
       val hub1 = new Hub(cols(0), cols(1))
       val hub2 = new Hub(cols(2), cols(3))
-      val path = new Hop(hub1, hub2, cols(4).toInt, cols(5).toInt, cols(6).toInt)
+      val hours = cols(4).toInt
+      val minutes = cols(5).toInt
+      val miles = cols(6).toInt
 
-      if (hubListDijkstra.keySet.exists(_ == hub1.id)) {
-        var tempList = new ListBuffer[(Double, String)]()
-        tempList = hubListDijkstra(hub1.id).to[ListBuffer]
-        val tuple2 = new Tuple2(path.miles.toDouble, hub2.id)
-        tempList += tuple2
-        hubListDijkstra.put (hub1.id,tempList.to[List])
-      }
-
-      else {
-        hubListDijkstra.put (hub1.id,List((path.miles.toDouble, hub2.id)))
-      }
-
-      if (hubListDijkstra.keySet.exists(_== hub2.id)) {
-        var tempList = new ListBuffer[(Double,String)]()
-        tempList = hubListDijkstra(hub2.id).to[ListBuffer]
-        val tuple2 = new Tuple2(path.miles.toDouble, hub1.id)
-        tempList += tuple2
-        hubListDijkstra.put (hub2.id,tempList.to[List])
-      }
-      else {
-        hubListDijkstra.put(hub2.id, List((path.miles.toDouble, hub1.id)))
-      }
-
+      putHop(hub1, hub2, hours, minutes, miles)
     }
-    source.close()
-    hubListDijkstra
   }
 
 }
